919f2ccd4d93894480f52dc990ac22077e3abb41
Vlads <Vladsinitsa23@gmail.com>
2022-03-22 14:28:36 +0300

    Delete yarn

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_URL: 'http://localhost:8080',
-    ID:'6338b801-4ed2-47e0-8a44-88f0d8da2ea2'
+    API_URL: 'https://bsu-application.herokuapp.com',
+    ID:'bd143f21-9130-46e4-b93b-30e3b68854a3'
   },
   webpack(config) {
     config.module.rules.push({
@@ -15,3 +15,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// env: {
+//   API_URL: 'https://bsu-application.herokuapp.com',
+//       ID:'bd143f21-9130-46e4-b93b-30e3b68854a3'
+//
+// API_URL: 'http://localhost:8080',
+//     ID:'6338b801-4ed2-47e0-8a44-88f0d8da2ea2'
+// },
